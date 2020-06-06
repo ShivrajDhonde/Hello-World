3bfd2f7fd942625f089dc4a12252dfdd08ff2fbc
Shivraj Dhonde <50475635+ShivrajDhonde@users.noreply.github.com>
2020-06-06 10:55:07 +0530

    Delete autograder 3.1.py

--- Python/autograder 3.1.py
+++
@@ -1,9 +0,0 @@
-hrs = input("Enter Hours:")
-h = float(hrs)
-rph = float(input("Enter the rate:"))
-if(h<=40):
-    gross_pay = rph*h
-else:
-    gross_pay = 40*rph + 1.5*rph*(h-40)
-    
-print(gross_pay)
